c018081645d5d187d052c374a4bd682382a27f8d
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-11 10:04:05 -0800

    Extract assert_equal method

--- test_fizzbuzz_of.rb
+++ test_fizzbuzz_of.rb
@@ -4,32 +4,36 @@ def assert(condition, message)
   raise "FAILURE:  " + message unless condition
 end
 
-expected = 1
-actual = fizzbuzz_of(1)
-assert actual == 1, "expected #{expected}, but got #{actual}"
+def assert_equal expected, actual
+  assert expected == actual, "expected #{expected}, but got #{actual}"
+end
 
 expected = 1
+actual = fizzbuzz_of(1)
+assert_equal expected, actual
+
+expected = 2
 actual = fizzbuzz_of(2)
-assert actual == 2, "expected #{expected}, but got #{actual}"
+assert_equal expected, actual
 
 expected = 'Fizz'
 actual = fizzbuzz_of(3)
-assert actual == 'Fizz', "expected #{expected}, but got #{actual}"
+assert_equal expected, actual
 
 expected = 'Buzz'
 actual = fizzbuzz_of(5)
-assert actual == 'Buzz', "expected #{expected}, but got #{actual}"
+assert_equal expected, actual
 
 expected = 'Fizz'
 actual = fizzbuzz_of(6)
-assert actual == 'Fizz', "expected #{expected}, but got #{actual}"
+assert_equal expected, actual
 
 expected = 'Buzz'
 actual = fizzbuzz_of(10)
-assert actual == 'Buzz', "expected #{expected}, but got #{actual}"
+assert_equal expected, actual
 
 expected = 'FizzBuzz'
 actual = fizzbuzz_of(15)
-assert actual == 'FizzBuzz', "expected #{expected}, but got #{actual}"
+assert_equal expected, actual
 
 puts "SUCCESS"
