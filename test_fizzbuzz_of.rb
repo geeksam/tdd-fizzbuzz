0ac451a588731fd10624277710a779defbf95644
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-11 09:58:36 -0800

    DRY up messages to #assert

--- test_fizzbuzz_of.rb
+++ test_fizzbuzz_of.rb
@@ -1,28 +1,28 @@
 require File.join(File.dirname(__FILE__), 'fizzbuzz_of')
 
 def assert(condition, message)
-  raise message unless condition
+  raise "FAILURE:  " + message unless condition
 end
 
 actual = fizzbuzz_of(1)
-assert actual == 1, "FAILURE:  expected 1, but got #{actual}"
+assert actual == 1, "expected 1, but got #{actual}"
 
 actual = fizzbuzz_of(2)
-assert actual == 2, "FAILURE:  expected 2, but got #{actual}"
+assert actual == 2, "expected 2, but got #{actual}"
 
 actual = fizzbuzz_of(3)
-assert actual == 'Fizz', "FAILURE:  expected 'Fizz', but got #{actual}"
+assert actual == 'Fizz', "expected 'Fizz', but got #{actual}"
 
 actual = fizzbuzz_of(5)
-assert actual == 'Buzz', "FAILURE:  expected 'Buzz', but got #{actual}"
+assert actual == 'Buzz', "expected 'Buzz', but got #{actual}"
 
 actual = fizzbuzz_of(6)
-assert actual == 'Fizz', "FAILURE:  expected 'Fizz', but got #{actual}"
+assert actual == 'Fizz', "expected 'Fizz', but got #{actual}"
 
 actual = fizzbuzz_of(10)
-assert actual == 'Buzz', "FAILURE:  expected 'Buzz', but got #{actual}"
+assert actual == 'Buzz', "expected 'Buzz', but got #{actual}"
 
 actual = fizzbuzz_of(15)
-assert actual == 'FizzBuzz', "FAILURE:  expected 'FizzBuzz', but got #{actual}"
+assert actual == 'FizzBuzz', "expected 'FizzBuzz', but got #{actual}"
 
 puts "SUCCESS"
