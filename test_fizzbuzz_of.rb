d908ee6e982116e17bca58207e0c7b557a78aef1
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-11 10:00:59 -0800

    Further DRY up messages to #assert

--- test_fizzbuzz_of.rb
+++ test_fizzbuzz_of.rb
@@ -4,25 +4,32 @@ def assert(condition, message)
   raise "FAILURE:  " + message unless condition
 end
 
+expected = 1
 actual = fizzbuzz_of(1)
-assert actual == 1, "expected 1, but got #{actual}"
+assert actual == 1, "expected #{expected}, but got #{actual}"
 
+expected = 1
 actual = fizzbuzz_of(2)
-assert actual == 2, "expected 2, but got #{actual}"
+assert actual == 2, "expected #{expected}, but got #{actual}"
 
+expected = 'Fizz'
 actual = fizzbuzz_of(3)
-assert actual == 'Fizz', "expected 'Fizz', but got #{actual}"
+assert actual == 'Fizz', "expected #{expected}, but got #{actual}"
 
+expected = 'Buzz'
 actual = fizzbuzz_of(5)
-assert actual == 'Buzz', "expected 'Buzz', but got #{actual}"
+assert actual == 'Buzz', "expected #{expected}, but got #{actual}"
 
+expected = 'Fizz'
 actual = fizzbuzz_of(6)
-assert actual == 'Fizz', "expected 'Fizz', but got #{actual}"
+assert actual == 'Fizz', "expected #{expected}, but got #{actual}"
 
+expected = 'Buzz'
 actual = fizzbuzz_of(10)
-assert actual == 'Buzz', "expected 'Buzz', but got #{actual}"
+assert actual == 'Buzz', "expected #{expected}, but got #{actual}"
 
+expected = 'FizzBuzz'
 actual = fizzbuzz_of(15)
-assert actual == 'FizzBuzz', "expected 'FizzBuzz', but got #{actual}"
+assert actual == 'FizzBuzz', "expected #{expected}, but got #{actual}"
 
 puts "SUCCESS"
