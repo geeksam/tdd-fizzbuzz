52e053bc091ce89882b0e9028c7144c4924dd518
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-11 09:50:38 -0800

    Whitespace

--- fizzbuzz_of.rb
+++ fizzbuzz_of.rb
@@ -2,13 +2,9 @@ def fizzbuzz_of(n)
   fizz = (n % 3) == 0
   buzz = (n % 5) == 0
   case
-  when fizz && buzz
-    'FizzBuzz'
-  when fizz
-    'Fizz'
-  when buzz
-    'Buzz'
-  else
-    n
+  when fizz && buzz ; 'FizzBuzz'
+  when fizz         ; 'Fizz'
+  when buzz         ; 'Buzz'
+  else              ; n
   end
 end
