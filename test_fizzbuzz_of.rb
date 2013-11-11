17e844c456c19a713af9b0fd3bb6c19501a22553
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-11 10:26:41 -0800

    Extract #test method to isolate state between tests

--- test_fizzbuzz_of.rb
+++ test_fizzbuzz_of.rb
@@ -8,32 +8,50 @@ def assert_equal expected, actual
   assert expected == actual, "expected #{expected}, but got #{actual}"
 end
 
-expected = 1
-actual   = fizzbuzz_of(1)
-assert_equal expected, actual
+def test
+  yield
+end
 
-expected = 2
-actual   = fizzbuzz_of(2)
-assert_equal expected, actual
+test do
+  expected = 1
+  actual   = fizzbuzz_of(1)
+  assert_equal expected, actual
+end
 
-expected = 'Fizz'
-actual   = fizzbuzz_of(3)
-assert_equal expected, actual
+test do
+  expected = 2
+  actual   = fizzbuzz_of(2)
+  assert_equal expected, actual
+end
 
-expected = 'Buzz'
-actual   = fizzbuzz_of(5)
-assert_equal expected, actual
+test do
+  expected = 'Fizz'
+  actual   = fizzbuzz_of(3)
+  assert_equal expected, actual
+end
 
-expected = 'Fizz'
-actual   = fizzbuzz_of(6)
-assert_equal expected, actual
+test do
+  expected = 'Buzz'
+  actual   = fizzbuzz_of(5)
+  assert_equal expected, actual
+end
 
-expected = 'Buzz'
-actual   = fizzbuzz_of(10)
-assert_equal expected, actual
+test do
+  expected = 'Fizz'
+  actual   = fizzbuzz_of(6)
+  assert_equal expected, actual
+end
 
-expected = 'FizzBuzz'
-actual   = fizzbuzz_of(15)
-assert_equal expected, actual
+test do
+  expected = 'Buzz'
+  actual   = fizzbuzz_of(10)
+  assert_equal expected, actual
+end
+
+test do
+  expected = 'FizzBuzz'
+  actual   = fizzbuzz_of(15)
+  assert_equal expected, actual
+end
 
 puts "SUCCESS"
