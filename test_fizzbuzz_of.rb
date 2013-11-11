1a5683bfd12513adf45dded868e9375979e8895b
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-11 10:26:26 -0800

    Whitespace

--- test_fizzbuzz_of.rb
+++ test_fizzbuzz_of.rb
@@ -9,31 +9,31 @@ def assert_equal expected, actual
 end
 
 expected = 1
-actual = fizzbuzz_of(1)
+actual   = fizzbuzz_of(1)
 assert_equal expected, actual
 
 expected = 2
-actual = fizzbuzz_of(2)
+actual   = fizzbuzz_of(2)
 assert_equal expected, actual
 
 expected = 'Fizz'
-actual = fizzbuzz_of(3)
+actual   = fizzbuzz_of(3)
 assert_equal expected, actual
 
 expected = 'Buzz'
-actual = fizzbuzz_of(5)
+actual   = fizzbuzz_of(5)
 assert_equal expected, actual
 
 expected = 'Fizz'
-actual = fizzbuzz_of(6)
+actual   = fizzbuzz_of(6)
 assert_equal expected, actual
 
 expected = 'Buzz'
-actual = fizzbuzz_of(10)
+actual   = fizzbuzz_of(10)
 assert_equal expected, actual
 
 expected = 'FizzBuzz'
-actual = fizzbuzz_of(15)
+actual   = fizzbuzz_of(15)
 assert_equal expected, actual
 
 puts "SUCCESS"
