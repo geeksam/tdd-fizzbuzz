d77b696298969d30af8309b72ac78dfc0a349691
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-17 21:21:50 -0800

    Fill in some more tests

--- test_fizzbuzz_of.rb
+++ test_fizzbuzz_of.rb
@@ -37,8 +37,17 @@ require File.join(File.dirname(__FILE__), 'fizzbuzz_of')
 test_fizzbuzz 1,  1
 test_fizzbuzz 2,  2
 test_fizzbuzz 3,  'Fizz'
+test_fizzbuzz 4,  4
 test_fizzbuzz 5,  'Buzz'
+test_fizzbuzz 6,  'Fizz'
+test_fizzbuzz 7,  7
+test_fizzbuzz 8,  8
+test_fizzbuzz 9,  'Fizz'
 test_fizzbuzz 10, 'Buzz'
+test_fizzbuzz 11,  11
+test_fizzbuzz 12,  'Fizz'
+test_fizzbuzz 13,  13
+test_fizzbuzz 14,  14
 test_fizzbuzz 15, 'FizzBuzz'
 
 ##### RUNNING TESTS #####
